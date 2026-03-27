3baf47520e8363dde4dcac3fb5006a3afe46d1e8
cpy <chenpengyu1123@163.com>
2026-03-27 10:35:53 +0800

    完成后端开发用户模块

---
+++ src/main/java/com/oxiris/yuaicodemother/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.oxiris.yuaicodemother.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.oxiris.yuaicodemother.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
