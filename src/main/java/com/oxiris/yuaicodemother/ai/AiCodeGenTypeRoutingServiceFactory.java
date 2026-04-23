83881bf9dbceb479da3fb50d40a6c8bbcb778b63
cpy <chenpengyu1123@163.com>
2026-04-23 21:52:40 +0800

    零代码应用生成平台--微服务改造-1、通用模块（提交为中间态，影响单体项目，不要单独使用当前提交），删除了多余的CacheKeyUtils.java和SpringContextUtil.java

--- src/main/java/com/oxiris/yuaicodemother/ai/AiCodeGenTypeRoutingServiceFactory.java
+++ src/main/java/com/oxiris/yuaicodemother/ai/AiCodeGenTypeRoutingServiceFactory.java
@@ -1,9 +1,7 @@
 package com.oxiris.yuaicodemother.ai;
 
-import com.oxiris.yuaicodemother.utils.SpringContextUtil;
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.service.AiServices;
-import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
